abfbbc7b1892a7191f121d107b47826a350bf5fd
Romulo Freitas <rfcontatosvia@gmail.com>
2023-05-09 19:34:08 -0300

    build

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,19 +7,66 @@ namespace Todo.Controllers{
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public List<TodoModel> Get([FromServices] AppDbContext context)
+        public IActionResult Get([FromServices] AppDbContext context)
         {
-            return context.TodoItems.ToList();
+            return Ok(context.TodoItems.ToList());
+        }
+
+         [HttpGet("/{id:int}")]
+        public IActionResult GetById(
+            [FromRoute] int id,
+            [FromServices] AppDbContext context)
+        {
+            var todos = context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (todos == null)
+                return NotFound();
+
+            return Ok(todos);
         }
 
         [HttpPost("/")]
-        public TodoModel Post(
+        public IActionResult Post(
         [FromBody] TodoModel model,
         [FromServices] AppDbContext context)
         {
             context.TodoItems.Add(model);
             context.SaveChanges();
-            return model;
+            return Created($"/{model.Id}", model);
+        }
+
+          [HttpPut("/{id:int}")]
+        public IActionResult Put(
+        [FromRoute] int id,
+        [FromBody] TodoModel todo,
+        [FromServices] AppDbContext context)
+        {
+            var model = context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+           model.Title = todo.Title;
+           model.Done =todo.Done;
+
+            context.TodoItems.Update(model);
+            context.SaveChanges();
+
+            return Ok(model);
+        }
+
+          [HttpDelete("/{id:int}")]
+        public IActionResult Delete(
+            [FromRoute] int id,
+        [FromServices] AppDbContext context)
+        {
+            var modelFromFrontend = context.TodoItems.FirstOrDefault(x => x.Id == id);
+            if (modelFromFrontend == null)
+                return NotFound();
+            context.TodoItems.Remove(modelFromFrontend);
+            context.SaveChanges();
+
+            return Ok(modelFromFrontend);
         }
     }
 }
