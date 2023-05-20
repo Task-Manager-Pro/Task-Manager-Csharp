cc028f2cedf167a90994e30c1ae0c58f9ffb205a
Romulo Freitas <rfcontatosvia@gmail.com>
2023-05-20 20:41:49 -0300

    cors config

--- Program.cs
+++ Program.cs
@@ -3,10 +3,22 @@ using Todo.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();  //informando aos asp.net core que vamos usar controllers
+
+//configurando o CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>();  //informando aos asp.net core que vamos usar o banco de dados
 
 
 var app = builder.Build();
-
+app.UseCors(); //informando aos asp.net core que vamos usar o CORS
 app.MapControllers(); 
 app.Run();
