33f83661560446cc7c871c57f0e7f7046c15a1e3
Romulo Freitas <rfcontatosvia@gmail.com>
2023-07-29 18:30:22 -0300

    [MIGRATION] novas propriedades

---
+++ Migrations/20230729212625_newModelToLogin.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Migrations
+{
+    /// <inheritdoc />
+    public partial class newModelToLogin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Login",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isLogged",
+                table: "Login",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Login");
+
+            migrationBuilder.DropColumn(
+                name: "isLogged",
+                table: "Login");
+        }
+    }
+}
--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -6,5 +6,7 @@ namespace Todo.Models
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool isLogged { get; set; }
     }
 }
