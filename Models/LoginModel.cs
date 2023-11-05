268563f27abf360a39fa168ec5008441658fabf7
Romulo Freitas <rfcontatosvia@gmail.com>
2023-11-05 17:35:19 -0300

    Feat: novo atributo na classe de loginModel para futuros Feats. Nova migration.

--- Models/LoginModel.cs
+++ Models/LoginModel.cs
@@ -2,11 +2,11 @@ namespace Todo.Models
 {
     public class LoginModel
     {
-        //hasnokey
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
         public bool isLogged { get; set; }
+        public DateTime LoginTime { get; set; } = DateTime.Now;
     }
 }
