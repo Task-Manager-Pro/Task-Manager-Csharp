1d1a922b96c7ddb42e059e1cedf3adf272b971ab
Romulo Freitas <rfcontatosvia@gmail.com>
2023-11-05 17:34:30 -0300

    Refactor: Descomentando linha do constructor  do AppDbContext para fins de configuração do Bearer Token

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Todo.Models;
 
 namespace Todo.Data {
     public class AppDbContext : DbContext {
-       // public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<TodoModel> TodoItems { get; set; }
 
         public DbSet<TodoModelPlus> TodoItemsPlus { get; set; }
