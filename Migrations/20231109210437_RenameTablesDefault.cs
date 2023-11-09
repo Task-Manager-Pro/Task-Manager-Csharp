dfbb54f97b5210d005adeec6966b74bb5e7862d7
Romulo Freitas <rfcontatosvia@gmail.com>
2023-11-09 18:11:06 -0300

    Feat: Gerando nova migration para o contexto mais atual. Tabelas renomeadas e mapeamento do banco de dados correto.

---
+++ Migrations/20231109210437_RenameTablesDefault.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTablesDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+               name: "CategorieTask",
+               newName: "CategorieTaskEntity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
