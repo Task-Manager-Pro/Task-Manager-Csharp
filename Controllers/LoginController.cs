8ce8513a93e7d93c4ad7db5f438d49f766b260c9
Romulo Freitas <rfcontatosvia@gmail.com>
2023-11-09 12:40:02 -0300

    Refactor: removendo método desnecessário

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -93,29 +93,5 @@ namespace Todo.Controllers
 
             return tokenHandler.WriteToken(token);
         }
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors();
-            services.AddControllers();
-
-            var key = Encoding.ASCII.GetBytes(Settings.Secret);
-            services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });
-        }
     }
 }
