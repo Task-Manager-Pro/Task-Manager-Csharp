0b28a5137a59e6a08a3cdfb94ffb6dbb5c30ba6f
Romulo Freitas <rfcontatosvia@gmail.com>
2023-12-19 16:40:46 -0300

    Refactor: refatorando controller parte 4. Adicionando services. Regra: Controller pode no máximo chamar uma service.

--- Services/TaskManagerServices.cs
+++ Services/TaskManagerServices.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Todo.Data;
+using Todo.Models;
 
 namespace Todo.Services
 {
@@ -90,5 +91,63 @@ namespace Todo.Services
                 .ToList();
             return new OkObjectResult(tasks);
         }
+
+        public IActionResult GetById(int id)
+        {
+           TaskEntity task = context.Tasks.FirstOrDefault(x => x.Id == id);
+
+            if (task == null) return new NotFoundResult();
+
+            TaskEntity taskDetails = new TaskEntity()
+            {
+                Title = task.Title,
+                Description = task.Description,
+                CreatedAt = task.CreatedAt,
+                Done = task.Done
+            };
+
+            return new OkObjectResult(taskDetails);
+        }
+
+        public IActionResult InsertTask (TaskEntity model, int userId)
+        {
+            var category = context.CategorieTasks.FirstOrDefault(c => c.Id == model.CategorieTaskId);
+
+            if(category == null || model == null) return new BadRequestResult();
+
+            TaskEntity updateTask = new ()
+            {
+                Title = model.Title,
+                Description = model.Description,
+                Done = false,
+                CreatedAt = DateTime.Now,
+                CategorieTaskId = model.CategorieTaskId,
+                Category = context.CategorieTasks.FirstOrDefault(c => c.Id == model.CategorieTaskId),
+                UserId = userId
+            };
+
+            context.Tasks.Add(updateTask);
+            context.SaveChanges();
+
+            return new OkObjectResult(updateTask);
+        }
+
+        public IActionResult EditTask (TaskEntity model, int id)
+        {
+            TaskEntity taskToEdit = context.Tasks.FirstOrDefault(x => x.Id == id);
+
+            if (taskToEdit == null) return new NotFoundResult();
+
+            taskToEdit = new TaskEntity()
+            {
+                Title = model.Title,
+                Description = model.Description
+            };
+
+            context.Tasks.Update(taskToEdit);
+            context.SaveChanges();
+
+            return new OkObjectResult(taskToEdit);
+        }   
     }
 }
