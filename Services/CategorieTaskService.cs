7a2d9037bc95ce61156ad370eca50a3d6e5c87f7
Romulo Freitas <rfcontatosvia@gmail.com>
2023-12-31 22:43:47 -0300

    Refactor: Limpando controller de Categoria parte 1.  Controller poderá no máximo chamar uma service.

---
+++ Services/CategorieTaskService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+
+namespace Todo.Services
+{
+    public class CategorieTaskService
+    {
+        public readonly AppDbContext _context;
+
+        public CategorieTaskService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Get()
+        {
+            try
+            {
+                var categorieTasks = _context.CategorieTasks.ToList();
+                return new OkObjectResult(categorieTasks);
+            }
+            catch (Exception ex)
+            {
+                return new OkObjectResult("Não foi possível listar as categorias de tarefas.");
+            }
+        }
+
+    }
+}
